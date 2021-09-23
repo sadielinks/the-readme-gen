b92ee3cd07202d15afa1fa7fa5bac26583ec168b
Sadie Sial <sadiesial@gmail.com>
2021-09-22 22:23:51 -0500

    added variables necessary to use the questions that will be asked + also added said questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,64 @@
 // console.log('lets get started!')
+// ran npm init + npm install inquierer
 
 // declare variables
+const inquierer = require('inquirer');
+const fs = require('fs')
 
 
 // 1 - ask inquierer
+const questions = ([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Hi there,  what is your name?',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your projects name?',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please provide a brief description about your project:',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What will users need to install your code?',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'How are users supposed to use your project?',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'How can users test the code?',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "How can users contribute to the project's code?",
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your GitHub profile?',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your contact email?',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please provide the licensing used for your project:',
+    },
+])
 
 
 // 2 - append to HTML with template literals
