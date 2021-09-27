456bf8befd9d592453ace08f95eeadb0c5abacbc
Sadie Sial <sadiesial@gmail.com>
2021-09-26 19:34:31 -0500

    added a true testGenREADME.md to do checks before submission on bootcampspot

---
+++ README_examples/testGenREADME.md
@@ -0,0 +1,59 @@
+# EXAMPLE README Generator (Bootcamp Assignment 9 - Node.JS)
+
+By: Sadie Sial
+
+## Description:
+A basic application to help users generate a quality README.md document!
+
+![apache](https://img.shields.io/badge/license-APACHE%202.0-orange)
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Technologies](#technologies)
+* [Tests](#tests)
+* [Contributing](#contributing)
+* [Questions](#questions)
+* [License](#license)
+
+## Installation
+
+Users need to install the following:
+```
+npm i
+```
+
+## Usage
+
+```
+Run 'node index.js' in your command line. I am using the integrated terminal in my VS Code. Ensure you are in the correct folder you cloned/forked into.
+```
+
+## Technologies
+
+Javascript, JQuery, Dynamic HTML, & Node.js were used to create this project.
+
+## Tests
+
+Users can test this code by: 
+```
+After installation, run the program by answering the questions in your command line, and see if a 'GenREADME.md' file generates in the main folder.
+```
+
+## Contributing
+
+Would you like to contribute to the project?
+```
+Please contribute by git forking/cloning!
+```
+
+## Questions
+
+Do you have any questions or concerns? Please contact me at either my GitHub at [sadielinks](https://github.com/sadielinks) or email me at [sadiecodes@gmail.com](mailto:sadiecodes@gmail.com)
+
+## License
+
+The licensing for this project is provided by: [Apache 2.0 license](https://choosealicense.com/licenses/apache-2.0/).
+
+ A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,9 +5,9 @@
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
     return `![mit](https://img.shields.io/badge/license-MIT-brightgreen)`;
-  } else if (license === 'Apache 2.0') {
+  } else if (license === 'Apache') {
     return `![apache](https://img.shields.io/badge/license-APACHE%202.0-orange)`;
-  } else if (license === 'GNU GPLv3') {
+  } else if (license === 'GNU') {
     return `![gpl](https://img.shields.io/badge/license-GPL%203.0-green)`;
   } else if (license === 'Mozilla') {
     return `![bsd](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`;
@@ -21,9 +21,9 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   if (license === 'MIT') {
     return `[MIT license](https://choosealicense.com/licenses/mit/)`;
-  } else if (license === 'Apache 2.0') {
+  } else if (license === 'Apache') {
     return `[Apache 2.0 license](https://choosealicense.com/licenses/apache-2.0/)`;
-  } else if (license === 'GNU GPLv3') {
+  } else if (license === 'GNU') {
     return `[GNU GPLv3 license](https://choosealicense.com/licenses/agpl-3.0/)`;
   } else if (license === 'Mozilla') {
     return `[Mozilla license](https://opensource.org/licenses/MPL-2.0')`;
@@ -37,9 +37,9 @@ function renderLicenseLink(license) {
 function renderLicenseSection(license) {
   if (license === 'MIT') {
     return ` A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code. `;
-  } else if (license === 'Apache 2.0') {
+  } else if (license === 'Apache') {
     return ` A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`;
-  } else if (license === 'GNU GPLv3') {
+  } else if (license === 'GNU') {
     return ` Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. `;
   } else if (license === 'Mozilla') {
     return `Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. `;
